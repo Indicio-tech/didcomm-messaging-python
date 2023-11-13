979c6aa7c63bd24dcbeaa48d6dacc467fe8b2468
Daniel Bluhm <dbluhm@pm.me>
2023-11-13 11:00:26 -0500

    test: packaging service
    Failing at the moment
    
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

---
+++ tests/test_packaging.py
@@ -0,0 +1,69 @@
+"""Test PackagingService."""
+import pytest
+
+from aries_askar import Key, KeyAlg
+from didcomm_messaging.crypto.backend.askar import AskarCryptoService, AskarSecretKey
+from didcomm_messaging.crypto.backend.basic import InMemorySecretsManager
+from didcomm_messaging.packaging import PackagingService
+from didcomm_messaging.multiformats import multibase
+from didcomm_messaging.multiformats import multicodec
+from didcomm_messaging.resolver.peer import Peer2, Peer4
+from didcomm_messaging.resolver import PrefixResolver
+from did_peer_2 import KeySpec, generate
+
+
+@pytest.fixture
+def secrets():
+    """Fixture for secrets."""
+    yield InMemorySecretsManager()
+
+
+@pytest.fixture
+def crypto():
+    """Fixture for crypto."""
+    yield AskarCryptoService()
+
+
+@pytest.fixture
+def packaging(secrets, crypto):
+    """Fixture for packaging."""
+    yield PackagingService(
+        PrefixResolver({"did:peer:2": Peer2(), "did:peer:4": Peer4()}), crypto, secrets
+    )
+
+
+# TODO More thorough tests
+@pytest.mark.asyncio
+async def test_packer_basic(
+    secrets: InMemorySecretsManager,
+    crypto: AskarCryptoService,
+    packaging: PackagingService,
+):
+    """Test basic packaging.
+
+    This is a happy path test.
+    """
+    verkey = Key.generate(KeyAlg.ED25519)
+    xkey = Key.generate(KeyAlg.X25519)
+    did = generate(
+        [
+            KeySpec.verification(
+                multibase.encode(
+                    multicodec.wrap("ed25519-pub", verkey.get_public_bytes()),
+                    "base58btc",
+                )
+            ),
+            KeySpec.key_agreement(
+                multibase.encode(
+                    multicodec.wrap("x25519-pub", xkey.get_public_bytes()), "base58btc"
+                )
+            ),
+        ],
+        [],
+    )
+    await secrets.add_secret(AskarSecretKey(verkey, f"{did}#key-1"))
+    await secrets.add_secret(AskarSecretKey(xkey, f"{did}#key-2"))
+    message = b"hello world"
+    packed = await packaging.pack(message, [did], did)
+    unpacked = await packaging.unpack(packed)
+    assert unpacked == message
