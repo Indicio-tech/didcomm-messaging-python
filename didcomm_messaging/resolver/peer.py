cdfb56c56512f9abbce9ca84fcdf90eaec898f48
Colton Wolkins (Indicio work address) <colton@indicio.tech>
2023-12-06 15:46:16 -0700

    feat: Add method to check if did is resolvable
    Signed-off-by: Colton Wolkins (Indicio work address) <colton@indicio.tech>

--- didcomm_messaging/resolver/peer.py
+++ didcomm_messaging/resolver/peer.py
@@ -4,7 +4,10 @@ from didcomm_messaging.resolver import DIDResolver
 
 try:
     from did_peer_2 import resolve as resolve_peer_2
+    from did_peer_2 import PATTERN as peer_2_pattern
     from did_peer_4 import resolve as resolve_peer_4
+    from did_peer_4 import LONG_PATTERN as peer_4_pattern_long
+    from did_peer_4 import SHORT_PATTERN as peer_4_pattern_short
 except ImportError:
     raise ImportError(
         "did-peer-2 and did-peer-4 are required for did:peer resolution; "
@@ -15,6 +18,10 @@ except ImportError:
 class Peer2(DIDResolver):
     """did:peer:2 resolver."""
 
+    async def is_resolvable(self, did: str) -> bool:
+        """Check to see if a DID is resolvable."""
+        return peer_2_pattern.match(did)
+
     async def resolve(self, did: str) -> dict:
         """Resolve a did:peer:2 DID."""
         return resolve_peer_2(did)
@@ -22,6 +29,10 @@ class Peer2(DIDResolver):
 
 class Peer4(DIDResolver):
     """did:peer:4 resolver."""
+
+    async def is_resolvable(self, did: str) -> bool:
+        """Check to see if a DID is resolvable."""
+        return peer_4_pattern_short.match(did) or peer_4_pattern_long.match(did)
 
     async def resolve(self, did: str) -> dict:
         """Resolve a did:peer:4 DID."""
